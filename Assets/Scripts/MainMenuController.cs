02e84fa290892dcf944ec7a0aa4e9280cfcd3a7a
Bardirty <93084637+Bardirty@users.noreply.github.com>
2024-06-29 13:28:21 +0200

    InteractableCursorAdd

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
